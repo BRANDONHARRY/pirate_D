a670179bdba7092402e250e688223e17f581574d
Hambush2 <55706777+Hambush2@users.noreply.github.com>
2021-05-19 20:52:32 +0100

    Added Website Link Button
    Added a button to the main page of the game

---
+++ Pirate Treasure/Assets/Scripts/Start Menu Scripts/WebsiteLink.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebsiteLink : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void goToSite()
+    {
+        Application.OpenURL("http://web.socem.plymouth.ac.uk/COMP2003/COMP2003_D/website/public/index.php");
+    }
+}
